6a5633bf25efcc47d88f67af8775c5643267b11b
FatemehAria <ftemewh@gmail.com>
2024-08-23 22:35:37 +0330

    area var initialized, j var used to connect first and last points

--- calculate-area.js
+++ calculate-area.js
@@ -1,3 +1,8 @@
 const calculateArea = (points) => {
     const numberOfPoints = points.length;
+    let area = 0;
+
+    for(let i = 0; i < numberOfPoints; i++){
+        let j = (i + 1) % numberOfPoints;
+    }
 }
